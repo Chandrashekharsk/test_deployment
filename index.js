dd035b967e746aea36830db5c207831253702636
Chandrashekharsk <cs9981625252@gmail.com>
2024-09-27 00:22:30 +0530

    init

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/users', (req, res) => {
+  console.log("hello");
+  res.send('Users!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
